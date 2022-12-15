588511c961a70bd6c8e28a7bf0c73978d1654c8b
gaetanvan <gaetan.graphisme@hotmail.com>
2022-12-15 17:56:01 +0100

    1.1.0

--- index.php
+++ index.php
@@ -1,2 +1,61 @@
 <?php
-phpinfo();
+//Connexion a la BDD
+//Connexion
+//On stock les infos du user
+$user = [
+        "id"=>1,
+        "name"=>"Roger",
+        "age"=> 21,
+        "weight"=> 79,
+        "sex"=>"male",
+        "size"=>173,
+        "imc"=>23.4,
+        "mail"=>"roger@gmail.com",
+        "isLogged"=>true
+];
+$foods = [
+    "foodName"=>array("BigMac","Tomate","Wrap","Pizza","Céréales"),
+    "foodCalories"=>array(504,205,536,864,345),
+];
+if (!$user['isLogged']){
+    header('location: login.php');
+    exit;
+}
+$page = [
+        "title"=>"Alim-Entation"
+];
+include_once("includes/header.php");
+?>
+<body>
+    <div class="container">
+        <header>
+            <div class="title">Alim-Entation</div>
+            <div class="profil"><?php echo $user['name']; ?></div>
+        </header>
+        <section class="dataUser">
+            <div class="graph"></div>
+            <div class="imc"><?php echo $user['imc']; ?></div>
+            <div class="weight"><?php echo $user['weight']; ?> kg</div>
+        </section>
+        <section class="date">
+            <div class="date"><?php echo date("l M y") ?></div>
+        </section>
+        <section class="list">
+            <?php
+            for ($i = 0;$i < count($foods["foodName"]); $i++) {
+                echo
+                <div class="food" >
+                <div class="foodName" > Big Mac </div >
+                <div class="foodCalories" > 504 kcal </div >
+            </div >
+            }
+            ?>
+        </section>
+        <footer>
+            <button>+
+            </button>
+        </footer>
+    </div>
+    <?php
+    include_once 'includes/header.php';
+    ?>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+$page = [
+    "title"=>"Alim-Entation - Login"
+];
+include_once("includes/header.php");
+?>
+<?php
+include_once 'includes/header.php';
+?>
